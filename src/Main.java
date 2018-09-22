941e3963e655a045f70457f1e9d69dd541368fdc
AValov76 <43465649+AValov76@users.noreply.github.com>
2018-09-22 18:53:17 +0300

    приложение

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,57 @@
 import ru.kstovoservice.test.Test;
-public class Main {
-    public static void main(String... args){
-        System.out.println(new Test());
-        System.out.println(new Test());
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) {
+
+        Button button = new Button();
+        button.setText("Open a New Window");
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                Label secondLabel = new Label("I'm a Label on new Window");
+
+                StackPane secondaryLayout = new StackPane();
+                secondaryLayout.getChildren().add(secondLabel);
+
+                Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+                // New window (Stage)
+                Stage newWindow = new Stage();
+                newWindow.setTitle("Second Stage");
+                newWindow.setScene(secondScene);
+
+                // Set position of second window, related to primary window.
+                newWindow.setX(primaryStage.getX() + 200);
+                newWindow.setY(primaryStage.getY() + 100);
+
+                newWindow.show();
+            }
+        });
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+
+        Scene scene = new Scene(root, 450, 250);
+
+        primaryStage.setTitle("Front-Back обмен v 1.0");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main (String... args) {
+        //System.out.println(new Test());
+        launch(args);
     }
 }
