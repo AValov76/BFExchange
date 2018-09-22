46e460a1b32e3997b0168d4af60e80057ee39e5d
AValov76 <43465649+AValov76@users.noreply.github.com>
2018-09-22 16:52:17 +0300

    тестирую

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ import ru.kstovoservice.test.Test;
 public class Main {
     public static void main(String... args){
         System.out.println(new Test());
+        System.out.println(new Test());
     }
 }
