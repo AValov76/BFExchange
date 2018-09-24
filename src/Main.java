d92723cb029cb4657c93e74ba7842efa0af36c39
AValov76 <a_valov@mail.ru>
2018-09-24 15:32:58 +0300

    мелкие нюансы

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
                 StackPane secondaryLayout = new StackPane();
                 secondaryLayout.getChildren().add(label);
 
-                Scene secondScene = new Scene(secondaryLayout, 230, 190);
+                Scene secondScene = new Scene(secondaryLayout, 230, 130);
 
                 // New window (Stage)
                 Stage newWindow = new Stage();
