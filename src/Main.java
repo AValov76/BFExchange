3ff2d899eaf4852d26e16eef89b10b2b01137123
AValov76 <a_valov@mail.ru>
2018-09-24 15:36:49 +0300

    мелкие нюансы

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main extends Application {
 
     public void start (final Stage primaryStage) {
 
-        Button button = new Button();
-        button.setText("О программе");
+        Button button = new Button("О программе");
+        //button.setText("О программе");
 
         button.setOnAction(new EventHandler<ActionEvent>() {
 
