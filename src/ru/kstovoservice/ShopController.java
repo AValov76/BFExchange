7a9939c6d1f136b0add8ca7275f5076bc019b92f
AValov76 <43465649+AValov76@users.noreply.github.com>
2018-10-25 11:35:57 +0300

    продолжаем...

--- src/ru/kstovoservice/ShopController.java
+++ src/ru/kstovoservice/ShopController.java
@@ -1,33 +1,35 @@
 package ru.kstovoservice;
 
-import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.collections.ObservableList;
+
 import java.io.File;
 import java.net.URL;
-import java.util.*;
+import java.util.ResourceBundle;
 
 //ссылка на этот класс идёт в fxml файле Shop.fxml
 
 public class ShopController implements Initializable {
+
     @FXML
     public AnchorPane mainAnchorPane;
-    public Button buttonDir;
     public TextField dirExchange;
+    public TextField posName;
     public TextField rep;
     public TextField flag;
+    public Button buttonDir;
     public ChoiceBox frontChoiceBox;
 
     public void initialize (URL location, ResourceBundle resources) {
-
         //Кнопка выбора папки обмена
         buttonDir.setOnAction(event -> {
             final DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -35,11 +37,11 @@ public class ShopController implements Initializable {
             stage.initModality(Modality.APPLICATION_MODAL);
             File file = directoryChooser.showDialog(stage);
             if (file != null) {
-               dirExchange.setText(file.toString());
+                dirExchange.setText(file.toString());
             }
         });
         ObservableList items = FXCollections.observableArrayList();
-        items.addAll("Атол","Штрих");
+        items.addAll("Атол", "Штрих");
         frontChoiceBox.setItems(items);
         frontChoiceBox.getSelectionModel().selectFirst();
         //выбор типа Фронт-офиса кассы
