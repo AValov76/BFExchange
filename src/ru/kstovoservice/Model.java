6441d0577ecb53518a85e4ef3ae6db6b76c8874c
AValov76 <43465649+AValov76@users.noreply.github.com>
2018-10-25 22:16:33 +0300

    Работа с моделью данных. Добавил:  - метод формирования файла с данными с нуля  - метод записи сформированного документа в файл  - метод добавления строки в документ на запись

--- src/ru/kstovoservice/Model.java
+++ src/ru/kstovoservice/Model.java
@@ -1,34 +1,25 @@
 package ru.kstovoservice;
 
-import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-interface IOData {
 
-    // Сохранение данных по магазинам в файл
-    boolean set ();
-
-    // Извлечение данных по магазинам в файл
-    boolean get ();
-}
 
 class POSDataModel {
 
     // собственно тут и хранится xml-запись
     String[] pos = new String[5]; //posName 0, pathPOS 1, typeofPOS 2, repName 3, flagName 4
-    final static String[] POS = {"Касса №1", "C:\\Obmen","Атол","report.rep","report.flg"};
+    final static String[] POS = {"Касса №1", "C:\\Obmen", "Атол", "report.rep", "report.flg"};
 
     POSDataModel () {
         init();
@@ -87,7 +78,7 @@ class POSDataModel {
     }
 }
 
-public class Model implements IOData {
+public class Model {
     private static final String FILENAME = "company.xml";
 
     public Model () {
@@ -95,77 +86,77 @@ public class Model implements IOData {
         posinit();
     }
 
-    void posinit() {
+    private void addPOS (POSDataModel newpos, Document document, Element company) {
 
+        // Элемент типа pos
+        Element posName = document.createElement("posName");
+        company.appendChild(posName);
+
+        // Еще можно сделать так
+        posName.setAttribute("posName", newpos.getPosName());
+
+        // Определяем posName
+        Element pathPOS = document.createElement("pathPOS");
+        pathPOS.setTextContent(newpos.getPathPOS());
+        posName.appendChild(pathPOS);
+
+        // Определяем typeofPOS
+        Element typeofPOS = document.createElement("typeofPOS");
+        typeofPOS.setTextContent(newpos.getTypeofPOS());
+        posName.appendChild(typeofPOS);
+
+        // Определяем repName
+        Element repName = document.createElement("repName");
+        repName.setTextContent(newpos.getRepName());
+        posName.appendChild(repName);
+
+        // Определяем flagName
+        Element flagName = document.createElement("flagName");
+        flagName.setTextContent(newpos.getFlagName());
+        posName.appendChild(flagName);
+
+    }
+    private void saveData(Document document) throws ParserConfigurationException {
+
+        try{
+        // Сохранить текстовое представление XML документа в файл
+        Transformer transformer = TransformerFactory.newInstance()
+                .newTransformer();
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(
+                new File(System.getProperty("user.dir")
+                        + File.separator + FILENAME));
+
+        // Для соображений отладки можно вывести результат работы
+        // программы на стандартный вывод
+        // StreamResult result = new StreamResult(System.out);
+
+        transformer.transform(source, result);
+
+        System.out.println("Документ сохранен!");
+
+    } catch (TransformerException ex) {
+        Logger.getLogger(Model.class.getName())
+                .log(Level.SEVERE, null, ex);
+    }
+}
+    void posinit () {
         // готовим данные по умолчанию для создания исходного файла
-        POSDataModel newpos = new POSDataModel();
-
         try {
             Document document = DocumentBuilderFactory.newInstance()
                     .newDocumentBuilder().newDocument();
-
             // Корневой элемент
             Element company = document.createElement("company");
             document.appendChild(company);
+            // добавляем 2 новых строки в документ
+            addPOS(new POSDataModel(), document,company);
+            addPOS(new POSDataModel(), document,company);
+            // сохраняем документ
+            saveData(document);
 
-            // Элемент типа pos
-            Element posName = document.createElement("posName");
-            company.appendChild(posName);
-
-            // Еще можно сделать так
-             posName.setAttribute("posName", newpos.getPosName());
-
-            // Определяем posName
-            Element pathPOS = document.createElement("pathPOS");
-            pathPOS.setTextContent(newpos.getPathPOS());
-            posName.appendChild(pathPOS);
-
-            // Определяем typeofPOS
-            Element typeofPOS = document.createElement("typeofPOS");
-            typeofPOS.setTextContent(newpos.getTypeofPOS());
-            posName.appendChild(typeofPOS);
-
-            // Определяем repName
-            Element repName = document.createElement("repName");
-            repName.setTextContent(newpos.getRepName());
-            posName.appendChild(repName);
-
-            // Определяем flagName
-            Element flagName = document.createElement("flagName");
-            flagName.setTextContent(newpos.getFlagName());
-            posName.appendChild(flagName);
-
-            // Сохранить текстовое представление XML документа в файл
-            Transformer transformer = TransformerFactory.newInstance()
-                    .newTransformer();
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(
-                    new File(System.getProperty("user.dir")
-                            + File.separator + FILENAME));
-
-            // Для соображений отладки можно вывести результат работы
-            // программы на стандартный вывод
-            // StreamResult result = new StreamResult(System.out);
-
-            transformer.transform(source, result);
-
-            System.out.println("Документ сохранен!");
-
-        } catch (ParserConfigurationException
-                | TransformerConfigurationException ex) {
-            Logger.getLogger(Model.class.getName())
-                    .log(Level.SEVERE, null, ex);
-        } catch (TransformerException ex) {
+        } catch (ParserConfigurationException ex) {
             Logger.getLogger(Model.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
-    }
-
-    public boolean set () {
-        return true;
-    }
-
-    public boolean get () {
-        return true;
     }
 }
